f6fe27263d2ee0bb302ee78b4fc2e29bf64f9eac
mitch <miwa6095@colorado.edu>
2024-12-20 19:37:16 -0700

    more constants

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 # random constants to import other places
 
 AVOGADRO = 6.0225e23  # Avogadro's number
-ACOGADRO_SI = 6.0225e26
+AVOGADRO_SI = 6.0225e26
 BOLTZMANN = 1.381e-23  # Boltzmann constant
 STEFBOLTZ = 5.670374419e-8 # stefan-boltzmann constant
 GASCON = 8.3143  # J/mol-K?
